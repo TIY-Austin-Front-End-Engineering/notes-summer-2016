737ee7ba3e7e64b088975e58d4e80a3f28d90637
Jess Scheuring <jscheuring@gmail.com>
2016-06-30 12:36:00 -0500

    basic app for day 14

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('hi');
+var data = [
+  {
+    name: 'Jess',
+    messages: ['hi, Jess', 'you always talk about unicorns']
+  },
+  {
+    name: 'Quincy',
+    messages: ['you always talk about sports', 'you love sports']
+  },
+  {
+    name: 'Karly',
+    messages: ['i know you lvoe running']
+  }
+];
+
+var personLink = '<li><a href="#"></a></li>';
+var personMessages = '<div><h2>Person\'s Name</h2><ul><li>Message</li><li>Message</li><li>Message</li></ul></div>';
+
+// listen to click events on the people-list
+// know who was clicked on so we can show their name and messages
+
+$(document).ready(function() {
+  // code will only run after the entire docuemnt is loaded and ready
+  console.log('documentReady');
+
+  var parent = $('.people-list');
+  data.forEach(function(person, i) {
+    var $newEl = $(personLink);
+    $newEl.children('a').text(person.name).attr('data-index', i);
+    parent.append($newEl);
+  });
+
+  parent.on('click', function(evt) {
+    var index = $(evt.target).data().index;
+    var $newEl = $(personMessages);
+    $newEl.children('h2').text(data[index].name);
+    $('.messages-area').append($newEl);
+  });
+});
